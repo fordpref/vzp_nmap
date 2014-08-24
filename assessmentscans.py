1e34db9b51d7a9f51a0083d154fa5b75990bd64c
fordpref <mike@ermanfamily.com>
2014-08-24 16:43:09 -0400

    2014.08.24
    Added an OS report to the parsed output.
    it truncates some of the nmap output to 45 characters, and gives a few
    other tidbits for the os.

--- assessmentscans.py
+++ assessmentscans.py
@@ -22,6 +22,7 @@ files = []
 proto = {}
 opsys = platform.system()
 hostlistfile = ''
+parseos = {}
 
 
 def startup():
@@ -276,9 +277,9 @@ def udpscans():
     os.system(clear)
     print '\n\nPerforming UDP Syn Scans\n'
     if opsys == 'Windows':
-        call('nmap.exe -Pn -n --open -sU --top-ports 30 -sV -sC --max-hostgroup 10 --max-retries 3 --host-timeout 2m -iL ' + hostlistfile + ' -oA ' + tempfile)
+        call('nmap.exe -Pn -n --open -sU --top-ports 20 -sV -sC --max-hostgroup 10 --max-retries 3 --host-timeout 2m -iL ' + hostlistfile + ' -oA ' + tempfile)
     else:
-        call('nmap -Pn -n --open -sU --top-ports 30 -sV -sC --max-hostgroup 10 --max-retries 3 --host-timeout 2m -iL ' + hostlistfile + ' -oA ' + tempfile, shell = True)
+        call('nmap -Pn -n --open -sU --top-ports 20 -sV -sC --max-hostgroup 10 --max-retries 3 --host-timeout 2m -iL ' + hostlistfile + ' -oA ' + tempfile, shell = True)
 
 def ipscans():
     global opsys, hostlist, nmapdir, clear
@@ -318,195 +319,255 @@ def portreportprep():
                print 'Failed to parse: ' + nmapdir + x
     
 def parse():
-   """
-   This is a bit weird.  We want to capture each tcp and udp port number
-   we find listening, capture the service name (there may be multiple), capture
-   the productname (there may be multiple), and capture the IP addresses for each port and service.
-   So, 21 may be open on 10 systems, but some are using wuftp, some
-   another ftp, some IIS.  In metasploit, I can list all the FTP IP addresses
-   but  I can't get a complete protocol/service name list.  I can get all the
-   listening services for a give host, but not all the ports from all the
-   hosts with all the services at once. This is what we are going to try to
-   do here.  proto['TCP']['##']['servicename']['productname'] = [ip list]
-   """
-   
-   global proto, root
+    """
+    This is a bit weird.  We want to capture each tcp and udp port number
+    we find listening, capture the service name (there may be multiple), capture
+    the productname (there may be multiple), and capture the IP addresses for each port and service.
+    So, 21 may be open on 10 systems, but some are using wuftp, some
+    another ftp, some IIS.  In metasploit, I can list all the FTP IP addresses
+    but  I can't get a complete protocol/service name list.  I can get all the
+    listening services for a give host, but not all the ports from all the
+    hosts with all the services at once. This is what we are going to try to
+    do here.  proto['TCP']['##']['servicename']['productname'] = [ip list]
+    """
 
-   protocol = ''
-   port = ''
-   servicename = ''
-   productname = ''
-   ipaddress = ''
-   addresses = []
+    global proto, root, parseos
 
-   #Now lets parse the xml and get the fields we need.
-   #The parent element is 'host'.  subelements will be nested below
-   #collected, tested for inclusion, and then added as needed.
-   for host in root.findall('host'):
-       addrobj = host.find('address')
-       ipaddress = addrobj.get('addr')
-       ipaddress = ''.join(ipaddress.split())
-       ports = host.find('ports')
-       if ports != None:
-           for x in ports.findall('port'):
-               protocol = x.get('protocol')
-               if protocol in proto:
-                   pass
-               else:
-                   proto[protocol] = {}
+    protocol = ''
+    port = ''
+    servicename = ''
+    productname = ''
+    ipaddress = ''
+    addresses = []
+    osobj = ''
+    osmatch = ''
+    tempos = ''
 
 
-               port = int(x.get('portid'))
-               if port in proto[protocol]:
-                   pass
-               else:
-                   proto[protocol][port] = {}
+    #Now lets parse the xml and get the fields we need.
+    #The parent element is 'host'.  subelements will be nested below
+    #collected, tested for inclusion, and then added as needed.
+    for host in root.findall('host'):
+        addrobj = host.find('address')
+        ipaddress = addrobj.get('addr')
+        ipaddress = ''.join(ipaddress.split())
+        ports = host.find('ports')
 
-               service = x.find('service')
-               if service != None:
+        if ports != None:
+            for x in ports.findall('port'):
+                protocol = x.get('protocol')
+                if protocol in proto:
+                    pass
+                else:
+                    proto[protocol] = {}
+
+
+                port = int(x.get('portid'))
+                if port in proto[protocol]:
+                    pass
+                else:
+                    proto[protocol][port] = {}
+
+                service = x.find('service')
+                if service != None:
                    
 
-                   if service.get('name') == None:
-                       servicename = 'None'
-                   else:
-                       servicename = service.get('name')
+                    if service.get('name') == None:
+                        servicename = 'None'
+                    else:
+                        servicename = service.get('name')
                        
-                   if servicename in proto[protocol][port]:
+                    if servicename in proto[protocol][port]:
+                        pass
+                    else:
+                        proto[protocol][port][servicename] = {}
+
+
+                    if service.get('product') == None:
+                        productname = 'None'
+                    else:
+                        productname = service.get('product')
+                       
+                    if productname in proto[protocol][port][servicename]:
+                        pass
+                    else:
+                        proto[protocol][port][servicename][productname] = {}
+                else:
+                    servicename = 'None'
+                    productname = 'None'
+                    if servicename in proto[protocol][port]:
                        pass
-                   else:
+                    else:
                        proto[protocol][port][servicename] = {}
 
-
-                   if service.get('product') == None:
-                       productname = 'None'
-                   else:
-                       productname = service.get('product')
-                       
-                   if productname in proto[protocol][port][servicename]:
+                    if productname in proto[protocol][port][servicename]:
                        pass
-                   else:
-                       proto[protocol][port][servicename][productname] = {}
-               else:
-                   servicename = 'None'
-                   productname = 'None'
-                   if servicename in proto[protocol][port]:
-                       pass
-                   else:
-                       proto[protocol][port][servicename] = {}
-
-                   if productname in proto[protocol][port][servicename]:
-                       pass
-                   else:
+                    else:
                        proto[protocol][port][servicename][productname] = {}
 
                
-           
-               if 'IP' in proto[protocol][port][servicename][productname]:
-                   if ipaddress in proto[protocol][port][servicename][productname]['IP']:
-                       pass
-                   else:
-                       proto[protocol][port][servicename][productname]['IP'].append(ipaddress)
-               else:
-                   proto[protocol][port][servicename][productname]['IP'] = []
-                   proto[protocol][port][servicename][productname]['IP'].append(ipaddress)
+
+                if 'IP' in proto[protocol][port][servicename][productname]:
+                    if ipaddress in proto[protocol][port][servicename][productname]['IP']:
+                        pass
+                    else:
+                        proto[protocol][port][servicename][productname]['IP'].append(ipaddress)
+                else:
+                    proto[protocol][port][servicename][productname]['IP'] = []
+                    proto[protocol][port][servicename][productname]['IP'].append(ipaddress)
+
+        try:
+            osobj = host.find('os')
+            osmatch = osobj.find('osmatch')
+            tempos = osmatch.get('name')
+            tempos = ' '.join(tempos.split(','))
+            if len(tempos) > 45:
+                tempos = tempos[0:45]
+            ostype = ''
+            osvendor = ''
+            osfamily = ''
+            
+            if tempos in parseos:
+                if ipaddress in parseos[tempos]['ipaddress'] == False:
+                    parseos[tempos]['ipaddress'].append(ipaddress)
+            else:
+                parseos[tempos] = {}
+                parseos[tempos]['type'] = ''
+                parseos[tempos]['vendor'] = ''
+                parseos[tempos]['osfamily'] = ''
+                parseos[tempos]['ipaddress'] = []
+                parseos[tempos]['ipaddress'].append(ipaddress)
+                
+            for x in osmatch.findall('osclass'):
+                ostype = x.get('type')
+                osvendor = x.get('vendor')
+                osfamily = x.get('osfamily')
+                
+                if len(parseos[tempos]['type']) == 0:
+                    parseos[tempos]['type'] = ostype
+                elif ostype in parseos[tempos]['type']:
+                    pass
+                else:
+                    parseos[tempos]['type'] = parseos[tempos]['type'] + ' or ' + ostype
+                    
+                if len(parseos[tempos]['vendor']) == 0:
+                    parseos[tempos]['vendor'] = osvendor
+                elif osvendor in parseos[tempos]['vendor']:
+                    pass
+                else:
+                    parseos[tempos]['vendor'] = parseos[tempos]['vendor'] + ' or ' + osvendor
+                    
+                if len(parseos[tempos]['osfamily']) == 0:
+                    parseos[tempos]['osfamily'] = osfamily
+                elif osfamily in parseos[tempos]['osfamily']:
+                    pass
+                else:
+                    parseos[tempos]['osfamily'] = parseos[tempos]['osfamily'] + ' or ' + osfamily
+                    
+        except:
+            pass
+
 
 def remove_duplicates():
-   global proto
-   servicename = ''
-   productname = ''
-   sp = {}
-   s = {}
-   p = {}
-   popkey = []
-   removeip = []
-   
+    global proto
+    servicename = ''
+    productname = ''
+    sp = {}
+    s = {}
+    p = {}
+    popkey = []
+    removeip = []
 
-   for protocol in sorted(proto):
-       for port in sorted(proto[protocol]):
-           sp[port] = []
-           s[port] = []
-           p[port] = []
-           for servicename in proto[protocol][port]:
-               for productname in proto[protocol][port][servicename]:
-                   if servicename != 'None' and productname != 'None':
-                       sp[port].extend(proto[protocol][port][servicename][productname]['IP'])
-                   elif servicename !='None' and productname == 'None':
-                       s[port].extend(proto[protocol][port][servicename][productname]['IP'])
-                   elif servicename == 'None' and productname != 'None':
-                       p[port].extend(proto[protocol][port][servicename][productname]['IP'])
+
+    for protocol in sorted(proto):
+        for port in sorted(proto[protocol]):
+            sp[port] = []
+            s[port] = []
+            p[port] = []
+            for servicename in proto[protocol][port]:
+                for productname in proto[protocol][port][servicename]:
+                    if servicename != 'None' and productname != 'None':
+                        sp[port].extend(proto[protocol][port][servicename][productname]['IP'])
+                    elif servicename !='None' and productname == 'None':
+                        s[port].extend(proto[protocol][port][servicename][productname]['IP'])
+                    elif servicename == 'None' and productname != 'None':
+                        p[port].extend(proto[protocol][port][servicename][productname]['IP'])
 
        
-   
-   for protocol in sorted(proto):
-       for port in sorted(proto[protocol]):
-           for servicename in proto[protocol][port]:
-               for productname in proto[protocol][port][servicename]:
-                   if servicename == 'None' and productname == 'None':
-                       for ip in proto[protocol][port][servicename][productname]['IP']:
-                           if ip in sp[port]:
-                               removeip.append(ip)
-                           if ip in s[port]:
-                               removeip.append(ip)
-                           if ip in p[port]:
-                               removeip.append(ip)
-                       for ip in removeip:
-                           proto[protocol][port][servicename][productname]['IP'].remove(ip)
-                       removeip = []
-                   if servicename != 'None' and productname == 'None':
-                       for ip in proto[protocol][port][servicename][productname]['IP']:
-                           if ip in sp[port]:
-                               removeip.append(ip)
-                       for ip in removeip:
-                           proto[protocol][port][servicename][productname]['IP'].remove(ip)
-                       removeip = []
-                   if servicename == 'None' and productname != 'None':
-                       for ip in proto[protocol][port][servicename][productname]['IP']:
-                           if ip in sp[port]:
-                               removeip.append(ip)
-                       for ip in removeip:
-                           proto[protocol][port][servicename][productname]['IP'].remove(ip)
-                       removeip = []
-                           
-                           
-                       
-   
 
+    for protocol in sorted(proto):
+        for port in sorted(proto[protocol]):
+            for servicename in proto[protocol][port]:
+                for productname in proto[protocol][port][servicename]:
+                    if servicename == 'None' and productname == 'None':
+                        for ip in proto[protocol][port][servicename][productname]['IP']:
+                            if ip in sp[port]:
+                                removeip.append(ip)
+                            if ip in s[port]:
+                                removeip.append(ip)
+                            if ip in p[port]:
+                                removeip.append(ip)
+                        for ip in removeip:
+                            proto[protocol][port][servicename][productname]['IP'].remove(ip)
+                        removeip = []
+                    if servicename != 'None' and productname == 'None':
+                        for ip in proto[protocol][port][servicename][productname]['IP']:
+                            if ip in sp[port]:
+                                removeip.append(ip)
+                        for ip in removeip:
+                            proto[protocol][port][servicename][productname]['IP'].remove(ip)
+                        removeip = []
+                    if servicename == 'None' and productname != 'None':
+                        for ip in proto[protocol][port][servicename][productname]['IP']:
+                            if ip in sp[port]:
+                                removeip.append(ip)
+                        for ip in removeip:
+                            proto[protocol][port][servicename][productname]['IP'].remove(ip)
+                        removeip = []
    
 def report():
-   global proto, nmapdir
-
-   report = ''
-   date = date_time_stamp()
-   repfile1 = nmapdir + 'nmap-IPPROTO-port-report-' + date + '.csv'
-   repfile2 = nmapdir + 'nmap-TCP-port-report-' + date + '.csv'
-   repfile3 = nmapdir + 'nmap-UDP-port-report-' + date + '.csv'
-   writefile1 = open(repfile1, 'w')
-   writefile2 = open(repfile2, 'w')
-   writefile3 = open(repfile3, 'w')
-
-   writefile1.write('IP/TCP/UDP,Port#,Service Name,Product Name,IP List\n')
-   writefile2.write('IP/TCP/UDP,Port#,Service Name,Product Name,IP List\n')
-   writefile3.write('IP/TCP/UDP,Port#,Service Name,Product Name,IP List\n')
-   for protocol in sorted(proto):
-       for port in sorted(proto[protocol]):
-           for servicename in sorted(proto[protocol][port]):
-               for productname in sorted(proto[protocol][port][servicename]):
-                   if not proto[protocol][port][servicename][productname]['IP']:
-                       pass
-                   else:
-                       addresses = ",".join(proto[protocol][port][servicename][productname]['IP'])
-                       report = protocol + ',' + str(port) + ',' + servicename + ',' + productname + ',' + addresses + "\n"
-                       #print report
-                       if protocol == 'ip':
-                           writefile1.write(report)
-                       elif protocol == 'tcp':
-                           writefile2.write(report)
-                       else:
-                           writefile3.write(report)
-                    
-   writefile1.close()
-   writefile2.close()
-   writefile2.close()
+    global proto, nmapdir, parseos
+ 
+    report = ''
+    date = date_time_stamp()
+    repfile1 = nmapdir + 'nmap-IPPROTO-port-report-' + date + '.csv'
+    repfile2 = nmapdir + 'nmap-TCP-port-report-' + date + '.csv'
+    repfile3 = nmapdir + 'nmap-UDP-port-report-' + date + '.csv'
+    repfile4 = nmapdir + 'os-report-' + date + '.csv'
+    writefile1 = open(repfile1, 'w')
+    writefile2 = open(repfile2, 'w')
+    writefile3 = open(repfile3, 'w')
+    writefile4 = open(repfile4, 'w')
+ 
+    writefile1.write('IP/TCP/UDP,Port#,Service Name,Product Name,IP List\n')
+    writefile2.write('IP/TCP/UDP,Port#,Service Name,Product Name,IP List\n')
+    writefile3.write('IP/TCP/UDP,Port#,Service Name,Product Name,IP List\n')
+    writefile4.write('OSName,type,vendor,family,IP Addresses\n')
+    for protocol in sorted(proto):
+        for port in sorted(proto[protocol]):
+            for servicename in sorted(proto[protocol][port]):
+                for productname in sorted(proto[protocol][port][servicename]):
+                    if not proto[protocol][port][servicename][productname]['IP']:
+                        pass
+                    else:
+                        addresses = ",".join(proto[protocol][port][servicename][productname]['IP'])
+                        report = protocol + ',' + str(port) + ',' + servicename + ',' + productname + ',' + addresses + "\n"
+                        #print report
+                        if protocol == 'ip':
+                            writefile1.write(report)
+                        elif protocol == 'tcp':
+                            writefile2.write(report)
+                        else:
+                            writefile3.write(report)
+ 
+    for tempos in sorted(parseos):
+        addresses = ','.join(parseos[tempos]['ipaddress'])
+        report = tempos + ',' + parseos[tempos]['type'] + ',' + parseos[tempos]['vendor'] + ',' + parseos[tempos]['osfamily'] + ',' + addresses + '\n'
+        writefile4.write(report) 
+    writefile1.close()
+    writefile2.close()
+    writefile3.close()
+    writefile4.close()
 
            
     
